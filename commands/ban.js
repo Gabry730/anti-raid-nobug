347f2014196b50b775a042244ef7dbb6945f0aff
Gabry730 <68757104+Gabry730@users.noreply.github.com>
2020-08-09 01:23:57 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "ban the mentioned user",
 
     execute(bot, message, args) {
-        if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('You don't have permission to do that');
+        if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('You don\'t have permission to do that');
         var wUser = message.guild.member(message.mentions.users.first());
         if(!wUser) return message.reply('Non-existent user!');
         message.guild.member(wUser).ban().then(() => {
