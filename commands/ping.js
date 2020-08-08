08166754bff41b6f965261993d98716f98937d19
Gabry730 <68757104+Gabry730@users.noreply.github.com>
2020-08-09 00:59:55 +0200

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: 'ping',
-    description: 'questo comando ti fa vedere il suo ping,puo servire se il bot non risponde',
+    description: 'this command shows you its ping, it can be useful if the bot doesn\'t respond',
     execute(client, message, args) {
         const Discord = require("discord.js")
-        const Embed = new Discord.MessageEmbed().setTitle("Boi Status").setColor(message.member.displayHexColor).addFields(
+        const Embed = new Discord.MessageEmbed().setTitle("Bot Status").setColor(message.member.displayHexColor).addFields(
         {name:"Ping",value:client.ws.ping+"ms",inline:true},
         {name:"Uptime",value:client.uptime+" Milliseconds",inline:true},
         )
