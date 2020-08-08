7e19532ec96cb9994745f4e6c965878a3a200640
Gabry730 <68757104+Gabry730@users.noreply.github.com>
2020-08-09 00:54:38 +0200

    Update deletewarn.js

--- commands/deletewarn.js
+++ commands/deletewarn.js
@@ -3,10 +3,10 @@ let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports = {
     name: "deletewarn",
-    description: "togliere i warns all'utente mesionato",
+    description: "remove the warns from the user",
 
     execute(bot, message, args) {
-        if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('Non hai il permesso per farlo!');
+        if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('You don\'t have permission to do that!');
         let wUser = message.guild.member(message.mentions.users.first());
         if(!wUser) return message.reply("user non esistente!");
 
@@ -22,6 +22,6 @@ module.exports = {
 
         let warnlevel = warns[wUser.id].warns;
 
-        message.channel.send(`Ora <@${wUser.id}> ha **${warnlevel}** warning(s).`);
+        message.channel.send(`Now <@${wUser.id}> he has **${warnlevel}** warning(s).`);
     }
 }
